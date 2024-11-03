6ea36737b0621732906dcf63e9e502225be08683
Mhjba <mahjoubaaitalla15@gmail.com>
2024-11-03 16:32:09 +0100

    login and signup  users

---
+++ account/login.php
@@ -0,0 +1,57 @@
+<?php
+include('../conn.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <?php
+        @$email = $_POST['email'];
+        @$password = $_POST['password'];
+        @$add = $_POST['add'];
+        if (isset($add)) {
+            if (empty($email) || empty($password)) {
+                echo '<script>alert("Please enter your Email and Password");</script>';
+            } else {
+                $query = "SELECT * FROM account WHERE EMAIL='$email' AND password='$password'";
+                $result = mysqli_query($conn, $query);
+                if (mysqli_num_rows($result) == 1) {
+                    $_SESSION['EMAIL'] = $email;
+                    echo '<script>alert("WELCOME TO THE SHOPPING STORE.");</script>';
+                    header("REFRESH:2; URL=../user/index.php");
+                } else {
+                    echo '<script>alert("INCORRECT EMAIL OR PASSWORD.");</script>';
+                    header("REFRESH:2; URL=login.php");
+                }
+            }
+        }
+        ?>
+        <!----login container start --->
+            <div class="form-container">
+                <form action="" method="post">
+                    <h3>Log in</h3>
+                    <div class="input-group">
+                        <i class="fas fa-envelope"></i>
+                        <input type="email" name="email" required placeholder="Enter Email" class="box">
+                    </div>
+                    <div class="input-group">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" name="password" required placeholder="Enter Password" class="box">
+                    </div>
+                    <input type="submit" name="add" class="btn" value="Login">
+
+                    <p>Don't have an account? <a href="signup.php">Sign Up</a></p>
+                </form>
+            </div>
+        <!----login container End --->
+    </main>
+</body>
+</html>
+
---
+++ account/signup.php
@@ -0,0 +1,73 @@
+<?php
+include('../conn.php');
+
+if (isset($_POST['submit'])) {
+    @$username = $_POST['Name'];
+    @$email = $_POST['email'];
+    @$password = $_POST['password'];
+    @$copassword = $_POST['cpassword'];
+    @$query = mysqli_query($conn, "SELECT * FROM `account` WHERE email = '$email'");
+
+    if (mysqli_num_rows($query) > 0) {
+        echo '<script>alert("User already exists!");</script>';
+    } else {
+        mysqli_query($conn, "INSERT INTO `account`(username, email, password) VALUES('$username', '$email', '$password')");
+        echo '<script>alert("Registered successfully!");</script>';
+        header('location:login.php');
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Signup</title>
+</head>
+<body>
+    <!--- sinup container start ----->
+        <div class="form-container">
+            <form action="" method="post">
+                <h3>Sign Up</h3>
+                <!---Username---->
+                <div class="input-group">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="Name" required placeholder="Enter Username" class="box">
+                </div>
+                <!---email ---->
+                <div class="input-group">
+                    <i class="fas fa-envelope"></i>
+                    <input type="email" name="email" required placeholder="Enter Email" class="box">
+                </div>
+                <!---Password ---->
+                <div class="input-group">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" required placeholder="Enter Password" class="box">
+                </div>
+                <!---Confirm Password ---->
+                <div class="input-group">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="cpassword" required placeholder="Confirm Password" class="box">
+                </div>
+                <input type="submit" name="submit" class="btn" value="Sign up">
+                <!---social start ---->
+                    <div class="social-login">
+                            <p>Or sign up with:</p>
+                            <a href="https://www.facebook.com/" class="social-btn">
+                            <img src="../user/image/facebook.png"><h1></h1> 
+                            </a>
+                            <a href="https://accounts.google.com/" class="social-btn">
+                            <img src="../user/image/google.png"><h1></h1> 
+                            </a>
+                    </div>
+                <!---social start ---->
+                <p>Already have an account? <a href="login.php">Log in</a></p>
+            </form>
+        </div>
+    <!---sinup container End ----->
+</body>
+</html>
+
