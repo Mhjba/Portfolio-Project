94ca2f71fe4f1cb9d6d843ad28163203c115f402
Mhjba <mahjoubaaitalla15@gmail.com>
2024-11-03 17:06:29 +0100

    footer of home page

---
+++ home/footer.php
@@ -0,0 +1,76 @@
+    <!------------------------footer start-------------------------------->
+                <div class="back-top">
+                    <a href="#">Back to Top</a>
+                    <!-------lang start------->
+                        <div class="languages">
+                            <select id="lang">
+                                <option value="en">english</option>
+                                <option value="fr">franch</option>
+                                <option value="ar">arabic</option>
+                            </select>
+                        </div>
+                        <!-------lang end------->
+                </div>
+                <footer>
+                    <!-------about Us------->
+                    <div class="contanier">
+                        <div class="top-footer">
+                            <div class="small-row">
+                                <h6> Learn more about Us</h6>
+                                <div class="links-row">
+                                    <a href="#"><i class="fas fa-circle"></i></i>Information about Shopp</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Shipping and Delivery</a>
+                                </div>
+                            </div>
+                            <div class="small-row">
+                                <h6> Shop with us</h6>
+                                <div class="links-row">
+                                    <a href="#"><i class="fas fa-circle"></i></i>Your account</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Your purchases</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Your addresses</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Your Lists</a>
+                                </div>
+                            </div>
+                            <div class="small-row">
+                                <h6>Let us help you</h6>
+                                <div class="links-row">
+                                    <a href="#"><i class="fas fa-circle"></i></i>Help and Support</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Track order</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Return Policy</a>
+                                    <a href="#"><i class="fas fa-circle"></i></i>Terms and Conditions</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-------payment------->
+                        <div class="payment">
+                            <img src="image/payment-1.png" alt="">
+                            <img src="image/payment-2.png" alt="">
+                            <img src="image/payment-3.png" alt="">
+                            <img src="image/payment-4.png" alt="">
+                        </div>
+                    <!-------social start------->
+                        <div class="social">
+                            <ul>
+                                <li><a href="https://web.facebook.com/" target_blank><i class="fa-brands fa-facebook"></i></a></li>
+                                <li><a href="https://www.instagram.com/mh_jba?igsh=YzljYTk1ODg3Zg==" target_blank><i class="fa-brands fa-instagram"></i></a></li>
+                                <li><a href="" target_blank><i class="fa-brands fa-youtube"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-------social End------->
+                    <!------Copyright--------->
+                    <div class="bottom-footer">
+                        <div class="contanier">
+                            <p>Copyright &copy; 2024 All rights reserved | Made by <b>
+                                <a href="https://mhjba.com" target="_blank">Mhjba</a></b></p>
+
+                        </div>
+                    </div>
+                </footer>
+			<!-- Footer section Ends -->
+		</div>
+    <!------------------------footer End---------------------------------->
+
+</body>
+</html>
+
